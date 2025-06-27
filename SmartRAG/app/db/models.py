d2c2767f42c34d7827b5e070f2f0ef53c622adc2
Muhammad Awais <210633@students.au.edu.pk>
2025-06-27 18:51:01 +0500

    DB Model & Database added

---
+++ SmartRAG/app/db/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.types import JSON
+from datetime import datetime
+from db.database import Base
+
+class ChatLog(Base):
+    __tablename__ = "chat_logs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String, index=True)
+    query = Column(Text)
+    response = Column(Text)
+    metadata = Column(JSON)
+    timestamp = Column(DateTime, default=datetime.utcnow)
